809185538f9d4774b84b9f3484d0cafb4496a304
Craig R Webster <craig@barkingiguana.com>
2009-12-22 13:42:07 +0000

    Using spectacular as a submodule meant not storing spec files. Updated spectacular (now in vendor/spectacular) and added the missing spec file.

---
+++ spec/spec_email_message.js
@@ -0,0 +1,33 @@
+var email = require(path.join(process.cwd(), 'lib', 'email'));
+
+describe("An email message", function() {
+  beforeEach(function() {
+    basic_email = "To: alice\r\nFrom: bob\r\nSubject: Yo!\r\n\r\nLunch?\r\n";
+  });
+
+  it("should not change the raw source of a message", function() {
+    var message = email.message(basic_email);
+    assertEqual(basic_email, message.raw_source);
+  });
+
+  it("should be able to parse the 'To:' header", function() {
+    var message = email.message(basic_email);
+    assertEqual("alice", message.to);
+  });
+
+  it("should be able to parse the 'From:' header", function() {
+    var message = email.message(basic_email);
+    assertEqual("bob", message.from);
+  });
+
+  it("should be able to parse the 'Subject:' header", function() {
+    var message = email.message(basic_email);
+    assertEqual("Yo!", message.subject);
+  });
+
+  it("should be able to parse the body", function() {
+    var message = email.message(basic_email);
+    assertEqual("Lunch?", message.body);
+  });
+
+});
