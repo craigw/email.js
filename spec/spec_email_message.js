d37848bfc1d5b3e8d5ac0ff246a1a8938a36e9fa
Craig R Webster <craig@barkingiguana.com>
2010-02-02 13:48:38 +0000

    Allow fetching of arbitrary headers.

--- spec/spec_email_message.js
+++ spec/spec_email_message.js
@@ -2,7 +2,7 @@ var Email = require(path.join(process.cwd(), 'lib', 'email'));
 
 describe("An email message", function() {
   beforeEach(function() {
-    email_source = "To: alice\nFrom: bob\nSubject: Yo!\n\nLunch?\n";
+    email_source = "Time: Hammer Time\nTo: alice\nFrom: bob\nSubject: Yo!\n\nLunch?\n";
   });
 
   it("should not change the raw source of a message", function() {
@@ -15,34 +15,9 @@ describe("An email message", function() {
     assertEqual([ "alice" ], message.to());
   });
 
-  it("should be able to parse the 'From:' header", function() {
-    var message = new Email.Message(email_source);
-    assertEqual("bob", message.from());
-  });
-
-  it("should be able to parse the 'Subject:' header", function() {
-    var message = new Email.Message(email_source);
-    assertEqual("Yo!", message.subject());
-  });
-
-  it("should be able to parse the body", function() {
-    var message = new Email.Message(email_source);
-    assertEqual("Lunch?", message.body());
-  });
-});
-
-describe("An email message addressed to several people", function() {
-  beforeEach(function() {
-    email_source = "To: alice,emily\nFrom: bob\nSubject: Yo!\n\nLunch?\n";
-  });
-
-  it("should not change the raw source of a message", function() {
-    var message = new Email.Message(email_source);
-    assertEqual(email_source, message.raw_source());
-  });
-
-  it("should be able to parse the 'To:' header", function() {
-    var message = new Email.Message(email_source);
+  it("should be able to parse the 'To:' header even if there are several addresses", function() {
+    var source = "To: alice,emily\nFrom: bob\nSubject: Yo!\n\nTesting.";
+    var message = new Email.Message(source);
     assertEqual([ "alice", "emily" ], message.to());
   });
 
@@ -56,6 +31,11 @@ describe("An email message addressed to several people", function() {
     assertEqual("Yo!", message.subject());
   });
 
+  it("should be able to parse other headers", function() {
+    var message = new Email.Message(email_source);
+    assertEqual("Hammer Time", message.header("Time"));
+  });
+
   it("should be able to parse the body", function() {
     var message = new Email.Message(email_source);
     assertEqual("Lunch?", message.body());
@@ -64,6 +44,6 @@ describe("An email message addressed to several people", function() {
 
 describe("Creating a new email message from scratch", function() {
   it("should not set the to field", function() {
-    assertEqual([], new Email.Message().to())
+    assertEqual(undefined, new Email.Message().to())
   });
 })
